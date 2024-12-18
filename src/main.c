87a8408bdbd6e97b16e53cb7c924d64393acc3a0
Ethan Bertin--Prévot <ethan.bertin-prevot@epitech.eu>
2024-12-18 15:29:30 +0100

    Changed default ROM to brix.ch8 - 'pass'.

--- src/main.c
+++ src/main.c
@@ -71,7 +71,7 @@ int main()
     static Chip8 *chip;
 
     Chip8Utils.InitChip(&chip, wait_for_input, update_keys);
-    Chip8Utils.LoadChip(chip, "files/roms/test2.ch8");
+    Chip8Utils.LoadChip(chip, "files/roms/brix.ch8");
 
     Chip8Utils.set_seed(time(NULL));
 
